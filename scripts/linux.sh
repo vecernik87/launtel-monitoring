fe19c2b2028369f5fd01b7991ccea99b68e4d40d
vecernik87 <pavel@cernik.name>
2018-08-17 17:44:02 +1000

    Create linux.sh

---
+++ scripts/linux.sh
@@ -0,0 +1,1 @@
+wget launtel.troll-face.org/update.php --post-data="device=XXXXXXX&launtel=$(ping -c 5 45.248.48.1 | tail -1| awk -F '/' '{print $5}')&cloudflare=$(ping -c 5 1.1.1.1 | tail -1| awk -F '/' '{print $5}')&google=$(ping -c 5 8.8.8.8 | tail -1| awk -F '/' '{print $5}')"
